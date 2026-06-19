9a7e35a54bf5047d9e71a5d7f071254a3ad90c2c
Saif Khan <saifkhan@example.com>
2026-06-19 23:40:11 +0500

    Fix AUTH_USER_MODEL and token blacklist database issue

--- ignite/backend/backend/settings.py
+++ ignite/backend/backend/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AUTH_USER_MODEL = 'users.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
